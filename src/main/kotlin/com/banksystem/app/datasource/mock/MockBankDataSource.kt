67ae27a849462cbcfd9a4094454978c86b7a9a0a
Oleg Luganskiy <arky.happ@gmail.com>
2022-01-07 22:59:43 +0300

    feat (datasources/mock): add MockBankDataSource

---
+++ src/main/kotlin/com/banksystem/app/datasource/mock/MockBankDataSource.kt
@@ -0,0 +1,15 @@
+package com.banksystem.app.datasource.mock
+
+import com.banksystem.app.datasource.BankDataSource
+import com.banksystem.app.model.Bank
+import org.springframework.stereotype.Repository
+
+@Repository
+class MockBankDataSource: BankDataSource() {
+    val banks = listOf(
+        Bank("123", 3.14, 7),
+        Bank("1010", 2.03, 0),
+        Bank("234", 0.0, 97),
+    )
+    override fun retrieveBanks(): Collection<Bank> = banks
+}
