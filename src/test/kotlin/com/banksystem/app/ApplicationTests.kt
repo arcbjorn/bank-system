315993608ed21b4f0eae030b72150f1c66dd5c4b
Oleg Luganskiy <arky.happ@gmail.com>
2022-01-07 22:55:04 +0300

    init: Spring Boot project - Gradle, Kotlin & Mockk

---
+++ src/test/kotlin/com/banksystem/app/ApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.banksystem.app
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
