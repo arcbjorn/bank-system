29a7a9e1f529cd4197ca330f9a890c3a18104d73
Oleg Luganskiy <arky.happ@gmail.com>
2022-01-07 22:58:41 +0300

    test (services): BankService

---
+++ src/test/kotlin/com/banksystem/app/service/BankServiceTest.kt
@@ -0,0 +1,18 @@
+package com.banksystem.app.service
+
+import com.banksystem.app.datasource.BankDataSource
+import io.mockk.mockk
+import io.mockk.verify
+import org.junit.jupiter.api.Test
+
+internal class BankServiceTest {
+    private val dataSource : BankDataSource = mockk(relaxed = true)
+    private val bankService = BankService(dataSource)
+
+    @Test
+    fun `should call its data source to retrieve banks`() {
+//        every { dataSource.retrieveBanks() } returns emptyList()
+        bankService.getBanks()
+        verify(exactly = 1) { dataSource.retrieveBanks() }
+    }
+}
